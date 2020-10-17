5d006570a2b58558aafca414286eadbf56f51c81
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2020-10-16 23:24:27 -0300

    Melhorado a legenda dos outputs no gráfico

--- algoritmos.py
+++ algoritmos.py
@@ -6,7 +6,7 @@ algorithms = sys.argv[2].split(',')
 colors = ['blue', 'green', 'red', 'black']
 
 for i in range(len(algorithms)):
-    plt.plot([0, time[i]], ['início', 'fim'], label=algorithms[i].replace('Sort', ' Sort'), color=colors[i])
+    plt.plot([0, time[i]], ['início', 'fim'], label=f'{algorithms[i].replace("Sort", " Sort")} : {time[i]} milissegundos', color=colors[i]) 
 
 plt.ylabel('Início e fim da execução')
 plt.xlabel('Tempo em milissegundos para ordenação')
